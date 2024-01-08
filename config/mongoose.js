98d20cb8a8b5b83292455d8c6d24dfe21abcb0d4
Shruti <er.shrutimishra.2002@gmail.com>
2024-01-08 11:37:01 +0530

    email add

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-// const MONGO_URL = "mongodb+srv://pawnalakeholiday:%23PawnaLake123@pawnalake.apaxdtd.mongodb.net/?retryWrites=true&w=majority"
 const MONGO_URL = "mongodb://127.0.0.1/Oriphal_db"
 
 mongoose.connect(MONGO_URL);
